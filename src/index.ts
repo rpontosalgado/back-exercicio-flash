45ff3e29b41f55e6412e41424595ef9450e153f0
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2021-02-11 13:28:58 -0300

    Adicionando placeholder para as rotas

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
+// app.use("/company", companyRouter);
+// app.use("/employee", employeeRouter);
+
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
