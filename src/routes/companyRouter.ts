499418cebcdb3ca8fd1f21d9258cc90d88534a2f
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2021-02-11 13:42:39 -0300

    Adicionando rotas para as entidades empresa e funcionário

---
+++ src/routes/companyRouter.ts
@@ -0,0 +1,5 @@
+import express, { Router } from "express";
+
+export const companyRouter: Router = express.Router();
+
+// companyRouter.post("/", companyController.createCompany);
---
+++ src/routes/employeeRouter.ts
@@ -0,0 +1,7 @@
+import express, { Router } from "express";
+
+export const employeeRouter: Router = express.Router();
+
+// employeeRouter.post("/", employeeController.createEmployee);
+
+// employeeRouter.get("/:company", employeeController.getAllEmployeesByCompany);
